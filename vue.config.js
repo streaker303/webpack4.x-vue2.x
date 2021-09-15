1f8311fb2ac651c329e73971a41dba0e76e9786a
clearself <fuleiyang@qq.com>
2021-09-15 17:17:49 +0800

    兼容IE9+

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/dist/'
+        ? './'
         : '/',
     transpileDependencies: [
         /[/\\]node_modules[/\\](.+?)?element-ui(.*)[/\\]src/,
