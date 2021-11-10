bbee055d6adacb886809ccdf912e1da25289feed
streaker303 <1241362854@qq.com>
2021-11-10 14:28:40 +0800

    add error page

--- src/router/common.js
+++ src/router/common.js
@@ -1,8 +1,17 @@
 const login = () => import('@/pages/login/index.vue')
+const errorPage = () => import('@/pages/common/error.vue')
 export default [
     {
         path: '/',
         redirect: '/login'
+    },
+    {
+        path: '/error',
+        name: 'error',
+        component: errorPage,
+        meta: {
+            title: '系统出错了'
+        }
     },
     {
         path: '/login',
